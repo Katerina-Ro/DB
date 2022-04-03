75a9fe60593db9c9dff2e55ee206b0805a71dcce
Katerina <rabotahr2015@gmail.com>
2022-04-03 11:34:30 +0500

    db

---
+++ src/main/java/repositories/PassRepository.java
@@ -0,0 +1,16 @@
+package repositories;
+
+import enteties.Pass;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * {@link Repository для работы с сущностью {@link Pass }
+ */
+public interface PassRepository extends JpaRepository<Pass, Integer>{
+
+    @Query("SELECT p.chatId From Pass p WHERE p.numPass = :numPass")
+    Long findChatIdByPassId(@Param("numPass") Integer passId);
+}
